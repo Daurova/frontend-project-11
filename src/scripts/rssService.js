8b7f8336a317a92ed39251d0f054c11a600514bd
Daurova <147259989+Daurova@users.noreply.github.com>
2026-06-19 10:49:39 +0300

    fix/service fixed

--- src/scripts/rssService.js
+++ src/scripts/rssService.js
@@ -1,24 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-const PROXY_URL = 'https://allorigins.hexlet.app/get';
+const PROXY_URL = 'https://allorigins.hexlet.app/get'
 
 export const fetchRssFeed = (url) => {
-  const isLocal = url.startsWith('http://localhost') || url.startsWith('http://127.0.0.1') || url.startsWith('/');
-  if (isLocal) {
-    return axios.get(url).then(response => {
-      if (response.status !== 200) {
-        throw new Error('Network error');
-      }
-      return response.data;
-    });
-  }
-  
   const encodedUrl = encodeURIComponent(url);
   const requestUrl = `${PROXY_URL}?url=${encodedUrl}&disableCache=true`;
   return axios.get(requestUrl).then(response => {
     if (response.data.status?.http_code !== 200) {
       throw new Error('Network error');
     }
-    return response.data.contents;
+    console.log('response',response.data.content)
+    return response.data.content;
   });
 };
