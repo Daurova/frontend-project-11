a5144fa6e6c8279f5f1681661d2ade34f5e149b5
Daurova <147259989+Daurova@users.noreply.github.com>
2026-06-19 10:55:55 +0300

    fix/fixing servicce attempt

--- src/scripts/rssService.js
+++ src/scripts/rssService.js
@@ -1,15 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
-const PROXY_URL = 'https://allorigins.hexlet.app/get'
+const PROXY_URL = 'https://allorigins.hexlet.app/get';
 
 export const fetchRssFeed = (url) => {
   const encodedUrl = encodeURIComponent(url);
   const requestUrl = `${PROXY_URL}?url=${encodedUrl}&disableCache=true`;
-  return axios.get(requestUrl).then(response => {
-    if (response.data.status?.http_code !== 200) {
+  return axios.get(requestUrl)
+    .then(response => {
+      // Мок возвращает { contents: '...' }, реальный прокси тоже
+      if (response.data?.contents) {
+        return response.data.contents;
+      }
       throw new Error('Network error');
-    }
-    console.log('response',response.data.content)
-    return response.data.content;
-  });
+    })
+    .catch(() => {
+      throw new Error('Network error');
+    });
 };
